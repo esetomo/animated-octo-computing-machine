a68595f17b489544d36286d7ba8af52b95e6fcb7
Tomokazu Sato <s-tomo@s-tomo.jp>
2014-01-12 21:20:23 +0900

    UpdateLayeredWindowからアルファ付きImage処理に変更
    - DxLibのUpdateLayeredWindowではウィンドウ位置引数が指定されているためウィンドウが動かせませんでしたが、別方法とすることにより意図通り動くようになりました。
    - アルファ合成ではなく黒(0,0,0)が透過されていたのを、ちゃんとしたアルファ合成に修正しました。

--- Lemonade/Lemonade/MainWindow.xaml.cs
+++ Lemonade/Lemonade/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,10 +25,11 @@ namespace Lemonade
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int drawScreenHandle;
-        private int softImageHandle;
-        private int modelHandle;
-        private float a;
+        private int drawScreenHandle = -1;
+        private int softImageHandle = -1;
+        private int modelHandle = -1;
+        private float angle;
+        private WriteableBitmap bmp = null;
 
         public MainWindow()
         {
@@ -36,41 +38,57 @@ namespace Lemonade
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            HwndSource source = (HwndSource)HwndSource.FromVisual(this);
+            HwndSource source = new HwndSource(0, 0, 0, 0, 0, "DxLib", IntPtr.Zero);
             DX.SetUserWindow(source.Handle);
+
+            DX.SetUseBackBufferTransColorFlag(DX.TRUE);
 
             if (DX.DxLib_Init() == -1)
                 throw new Exception("DxLib Init failed");
-
-            drawScreenHandle = DX.MakeScreen((int)Width, (int)Height);
-            DX.SetDrawScreen(drawScreenHandle);
-
-            softImageHandle = DX.MakeARGB8ColorSoftImage((int)Width, (int)Height);
 
             string[] files = Directory.GetFiles(".", "*.pmd", SearchOption.AllDirectories);
             modelHandle = DX.MV1LoadModel(files[0]);
             DX.MV1SetPosition(modelHandle, DX.VGet(250.0f, 170.0f, -260.0f));
             DX.MV1SetScale(modelHandle, DX.VGet(8.0f, 8.0f, 8.0f));
-            a = 0.0f;
+            angle = 0.0f;
 
             CompositionTarget.Rendering += CompositionTarget_Rendering;
         }
 
         void CompositionTarget_Rendering(object sender, EventArgs e)
         {
+            int w = (int)Width;
+            int h = (int)Height;
+
+            if (bmp == null)
+            {
+                drawScreenHandle = DX.MakeScreen(w, h, DX.TRUE);
+                softImageHandle = DX.MakeSoftImage(w, h);
+                bmp = new WriteableBitmap(w, h, 96.0, 96.0, PixelFormats.Bgra32, null);
+                image1.Source = bmp;
+            }
+
+            DX.SetDrawScreen(drawScreenHandle);
             DX.ClearDrawScreen();
             RenderContent();
-
-            DX.GraphFilter(drawScreenHandle, DX.DX_GRAPH_FILTER_PREMUL_ALPHA);
-            DX.SetDrawScreen(drawScreenHandle);
             DX.GetDrawScreenSoftImage(0, 0, (int)Width, (int)Height, softImageHandle);
-            DX.UpdateLayerdWindowForPremultipliedAlphaSoftImage(softImageHandle);
+
+            bmp.Lock();
+            Int32Rect rect = new Int32Rect(0, 0, w, h);
+            IntPtr buf = dx_GetImageAddressSoftImage(softImageHandle);
+            bmp.WritePixels(rect, buf, w * h * 4, w * 4);
+            bmp.AddDirtyRect(rect);
+            bmp.Unlock();
         }
+
+        // DxLibDotNet.dllにある宣言はvoid *を使っておりunsafeのため、ポインタを使わないものを代わりに宣言
+        [DllImport("DxLib.dll")]
+        extern static IntPtr dx_GetImageAddressSoftImage(int SIHandle);
 
         private void RenderContent()
         {
-            a += 0.1f;
-            DX.MV1SetRotationXYZ(modelHandle, DX.VGet(0.0f, a, 0.0f));
+            angle += 0.1f;
+            DX.MV1SetRotationXYZ(modelHandle, DX.VGet(0.0f, angle, 0.0f));
             DX.MV1DrawModel(modelHandle);
         }
 
