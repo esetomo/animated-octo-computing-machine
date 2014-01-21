4fe9cd0d96868481700551c97702be6cbd0251bd
Ichigo Mayo <esetomo@gmail.com>
2014-01-21 19:06:49 +0900

    mmflexを使用している処理を削除しました

--- Lemonade/Lemonade/MainWindow.xaml.cs
+++ Lemonade/Lemonade/MainWindow.xaml.cs
@@ -5,10 +5,6 @@ using System.Collections.Generic;
 using System.Windows.Interop;
 using System.Diagnostics;
 using Microsoft.Win32;
-using MMF.CG.Model;
-using MMF.CG.Model.MMD;
-using MMF.CG.Motion;
-using MMDFileParser.PMXModelParser;
 
 namespace Lemonade
 {
@@ -17,8 +13,6 @@ namespace Lemonade
     /// </summary>
     public partial class MainWindow : Window
     {        
-        private MMDModel model;
-
         public MainWindow()
         {
             InitializeComponent();
@@ -30,29 +24,19 @@ namespace Lemonade
             if (Application.Current.MainWindow == this)
                 source.AddHook(new HwndSourceHook(SakuraAPI.WndProc));
 
-            // remove basic grid
-            if(renderControl.WorldSpace.DrawableResources.Count > 0)
-                renderControl.WorldSpace.RemoveResource(renderControl.WorldSpace.DrawableResources[0]);
-
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Filter = "pmxモデルファイル(*.pmx)|*.pmx";
             if (ofd.ShowDialog() != true)
                 return;
 
-            model = MMDModelWithPhysics.OpenLoad(ofd.FileName, renderControl.RenderContext);
-
             ofd.Filter = "vmdモーションファイル(*.vmd)|*.vmd";
             if (ofd.ShowDialog() == true)
             {
-                IMotionProvider motion = model.MotionManager.AddMotionFromFile(ofd.FileName, true);
-                model.MotionManager.ApplyMotion(motion, 0, ActionAfterMotion.Replay);
             }
             
-            renderControl.WorldSpace.AddResource(model);
-
             Dictionary<string, string> dic = new Dictionary<string, string>();
             dic["hwnd"] = source.Handle.ToString();
-            dic["name"] = model.Model.ModelInfo.ModelName_En;
+            dic["name"] = "noname";
             SakuraFMO.Save(dic);
         }
 
